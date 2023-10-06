60b50a60319a675b3e1a12258a2ecec2d2f88ab6
ola mohamed <olamohamedola276@gmail.com>
2023-10-06 14:12:11 -0700

    yaraab

---
+++ shell.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void exc(char *cmd, char **args);
+
+int main ()
+{
+	char buffer[1024];
+
+	while (1) 
+	{
+		printf("#cisfun$ ");
+
+	}
+}
+
+void exc(char *cmd, char **args)
+{
+	    
+}
