a3cb995dee335c24972749599359927a8cf89ae3
ola mohamed <olamohamedola276@gmail.com>
2023-10-17 16:39:27 -0700

    ola

--- shell.c
+++ shell.c
@@ -6,51 +6,50 @@
 
 void execdodo(char *cm, char *buffer)
 {
-	    char *args[64];
-	        int i = 0;
+	char *args[64];
+	int i = 0;
 
-		    args[i++] = cm;
-		        while ((cm = strtok(NULL, " \n")) != NULL)
-				    {
-					            args[i++] = cm;
-						        }
-			    args[i] = NULL;
+	args[i++] = cm;
+	while ((cm = strtok(NULL, " \n")) != NULL)
+	{
+		args[i++] = cm;
+	}
+	args[i] = NULL;
 
-			        execve(args[0], args, NULL);
-				    fprintf(stderr, "./hsh: 1: %s: not found\n", buffer); 
-				        _exit(127);
+	execve(args[0], args, NULL);
+	fprintf(stderr, "./hsh: 1: %s: not found\n", buffer);
+	_exit(127);
 }
 
 void excola(char *cm, char *buffer)
 {
-	    int iscomExists = access(cm, F_OK) == 0;
-	        int ifnotcombutpath = (cm[0] == '/') && (access(cm, X_OK) == 0);
-		    int status;
+	int iscomExists = access(cm, F_OK) == 0;
+	int ifnotcombutpath = (cm[0] == '/') && (access(cm, X_OK) == 0);
+	int status;
 
-		        if (iscomExists || ifnotcombutpath)
-				    {
-					            pid_t pid = fork();
-						            if (pid == -1)
-								            {
-										                perror("Error in forking");
-												            return;
-													            }
-							            if (pid == 0)
-									            {
-											                execdodo(cm, buffer);
-													        }
-								            else
-										            {
-												                waitpid(pid, &status, 0);
-														            if (WIFSIGNALED(status))
-																                {
-																			                write(2, "terminated by signal.\n", 22);
-																					            }
-															            }
-									        }
-			    else
-				        {
-						        fprintf(stderr, "./hsh: 1: %s: not found\n", cm);
-							    }
+	if (iscomExists || ifnotcombutpath)
+	{
+		pid_t pid = fork();
+		if (pid == -1)
+		{
+			perror("Error in forking");
+			return;
+		}
+		if (pid == 0)
+		{
+			execdodo(cm, buffer);
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+			if (WIFSIGNALED(status))
+			{
+				write(2, "terminated by signal.\n", 22);
+			}
+		}
+	}
+	else
+	{
+		fprintf(stderr, "./hsh: 1: %s: not found\n", cm);
+	}
 }
-
